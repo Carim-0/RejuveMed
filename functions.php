1bee3a1a0ff7e58989c9a5c849c65f75940183c8
Edwin Carim Ramirez Vega <edcaramirezve@ittepic.edu.mx>
2025-03-30 13:29:01 -0700

    Update functions.php

--- functions.php
+++ functions.php
@@ -1,23 +1,31 @@
 <?php
 
-    function check_login($con)
-    {
-        if(isset($_SESSION['user_id']))
-        {
-            $id = $_SESSION['user_id'];
-            $query = "select * from Pacientes where IDpaciente = '$id'";
+function check_login($con)
+{
+    if (isset($_SESSION['user_id']) && isset($_SESSION['user_type'])) {
+        $id = $_SESSION['user_id'];
+        $user_type = $_SESSION['user_type'];
 
-            $result = mysqli_query($con, $query);
-            if($result && mysqli_num_rows($result) > 0)
-            {
-                $user_data = mysqli_fetch_assoc($result);
-                return $user_data;
-            }
+        if ($user_type === 'Paciente') {
+            // revisar tabla pacientes
+            $query = "SELECT * FROM Pacientes WHERE IDpaciente = '$id'";
+        } elseif ($user_type === 'Personal') {
+            // revisar tabla personal
+            $query = "SELECT * FROM Personal WHERE IDpersonal = '$id'";
+        } else {
+            header("Location: login.php");
+            die;
         }
-        //Redirect to login
-        header("Location: login.php");
-        die;
+
+        $result = mysqli_query($con, $query);
+        if ($result && mysqli_num_rows($result) > 0) {
+            $user_data = mysqli_fetch_assoc($result);
+            return $user_data;
+        }
     }
 
-    
+    header("Location: login.php");
+    die;
+}
+
 ?>
