189a86349be020a489154006a224391556b925b6
AlixitoM <vstecnhology@gmail.com>
2025-04-01 16:49:04 -0700

    Update pacienteAgendarCita_Personal.php

--- Personal/pacienteAgendarCita_Personal.php
+++ Personal/pacienteAgendarCita_Personal.php
@@ -76,7 +76,6 @@
             border-radius: 10px;
             box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
             width: 400px;
-            box-shadow: var(0 4px 6px rgba(0, 0, 0, 0.1);)
         }
 
         .form-container h2 {
