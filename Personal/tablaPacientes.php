7d72bedd9c6c43b9e0bb40b970b38a8a884881c4
AlixitoM <vstecnhology@gmail.com>
2025-04-01 17:37:44 -0700

    Update tablaPacientes.php

--- Personal/tablaPacientes.php
+++ Personal/tablaPacientes.php
@@ -276,6 +276,10 @@
             </div>
         </div>
     </div>
+    <!-- Bottom buttons -->
+  <div class="bottom-buttons">
+    <button onclick="window.location.href='CtalogoRecepcionista.php'">Regresar</button>
+  </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
